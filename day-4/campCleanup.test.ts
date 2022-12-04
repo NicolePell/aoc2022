54af46686aa462d868f90ecc21d6dc0bf6e50c17
Nic Pell <nicole.pellicena@triptease.com>
2022-12-04 16:18:30 +0000

    Day Four: Part Two

--- day-4/campCleanup.test.ts
+++ day-4/campCleanup.test.ts
@@ -1,23 +1,23 @@
-import { campCleanup } from './campCleanup';
+import { campCleanupPartOne, campCleanupPartTwo } from './campCleanup';
 
 describe('DAY 4', () => {
   describe('PART 1', () => {
     test('Example', () => {
-      expect(campCleanup('day-4/example.txt')).toBe(2);
+      expect(campCleanupPartOne('day-4/example.txt')).toBe(2);
     });
 
     test('Input', () => {
-      expect(campCleanup('day-4/input.txt')).toBe(515);
+      expect(campCleanupPartOne('day-4/input.txt')).toBe(515);
     });
   });
 
-  xdescribe('PART 2', () => {
+  describe('PART 2', () => {
     test('Example', () => {
-      expect(campCleanup('day-4/example.txt')).toBe(false);
+      expect(campCleanupPartTwo('day-4/example.txt')).toBe(4);
     });
 
     test('Input', () => {
-      expect(campCleanup('day-4/input.txt')).toBe(false);
+      expect(campCleanupPartTwo('day-4/input.txt')).toBe(883);
     });
   });
 });
--- day-4/campCleanup.ts
+++ day-4/campCleanup.ts
@@ -1,24 +1,49 @@
 import { parseTextFile } from '../parseTextFile';
 
-export const campCleanup = (path: string) => {
+export const campCleanupPartOne = (path: string) => {
+  return getSections(path).map(calculateExactOverlaps()).reduce(calculateResult(), 0);
+};
+
+export const campCleanupPartTwo = (path: string) => {
+  return getSections(path).map(calculateAllOverlaps()).reduce(calculateResult(), 0);
+};
+
+function calculateExactOverlaps(): (pair: number[][]) => 1 | 0 {
+  return (pair: number[][]) => {
+    const [sectionOne, sectionTwo]: number[][] = pair;
+
+    const overlap = sectionOne.every((v) => sectionTwo.includes(v));
+    const overlapTwo = sectionTwo.every((v) => sectionOne.includes(v));
+
+    return overlap || overlapTwo ? 1 : 0;
+  };
+}
+
+function calculateAllOverlaps(): (pair: number[][]) => 1 | 0 {
+  return (pair: number[][]) => {
+    const [sectionOne, sectionTwo]: number[][] = pair;
+
+    const overlap = sectionOne.some((v) => sectionTwo.includes(v));
+    const overlapTwo = sectionTwo.some((v) => sectionOne.includes(v));
+
+    return overlap || overlapTwo ? 1 : 0;
+  };
+}
+
+const range = (start: number, stop: number, step = 1) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+function getSections(path: string) {
   return parseTextFile(path)
     .split('\n')
     .filter((val) => val !== '')
     .map((pairs) => pairs.split(','))
     .map((pair) => pair.map((section) => section.split('-').map((section) => Number.parseInt(section))))
-    .map((pair) => pair.map((section) => range(section[0], section[1])))
-    .map((pair) => {
-      const [sectionOne, sectionTwo]: number[][] = pair;
+    .map((pair) => pair.map((section) => range(section[0], section[1])));
+}
 
-      const overlap = sectionOne.every((v) => sectionTwo.includes(v));
-      const overlapTwo = sectionTwo.every((v) => sectionOne.includes(v));
-
-      return overlap || overlapTwo ? 1 : 0;
-    })
-    .reduce((previousValue: number, currentValue: number) => {
-      return previousValue + currentValue;
-    }, 0);
-};
-
-const range = (start: number, stop: number, step = 1) =>
-  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+function calculateResult() {
+  return (previousValue: number, currentValue: number) => {
+    return previousValue + currentValue;
+  };
+}
